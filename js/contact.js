4effec3946c3c34b3cf337f635c71ec86c47c075
eleung32 <52718704+eleung32@users.noreply.github.com>
2021-08-14 15:01:08 -0400

    added js to hide contact info, fine-tuned footer layout

---
+++ js/contact.js
@@ -0,0 +1,14 @@
+//hide phone number
+const phoneNumber1 = "+1 416 587 0971";
+document.getElementById("phone1").innerHTML=`${phoneNumber1}`;
+// document.getElementById("callme").innerHTML=`<a href="tel:${phoneNumber}">${phoneNumber}</a>`;
+const phoneNumber2 = "+1 416 587 0971";
+document.getElementById("phone2").innerHTML=`${phoneNumber2}`;
+
+
+//hide email address
+const emailAddress1 = "rochelle@mgbmc.com";
+document.getElementById("sendmail1").innerHTML=`${emailAddress1}`;
+// document.getElementById("sendmail").innerHTML=`<a href="mailto:${emailAddress}">${emailAddress}</a>`;
+const emailAddress2 = "rochelle@mgbmc.com";
+document.getElementById("sendmail2").innerHTML=`${emailAddress2}`;
