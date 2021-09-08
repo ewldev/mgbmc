d3e3b3747ae6b84ed7628782c0f090041316a8c0
eleung32 <52718704+eleung32@users.noreply.github.com>
2021-09-08 12:37:01 -0400

    image alt tags and house keeping

---
+++ js/run_slick.js
@@ -0,0 +1,22 @@
+//Slick carousel plugin - https://kenwheeler.github.io/slick/
+
+     $(document).ready(function(){
+          $('.client-logos').slick({
+            // centerMode:true,
+            
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            autoplay: true,
+            arrows: false,
+           
+            // autoplaySpeed: 2000,
+            // variableWidth: true,
+            // variableHeight: true    
+          });
+        });
+
+       
+
+ 
+
+    
