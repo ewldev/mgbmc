2c914634e1bbee4bc98e207b1d156a2f146d80c1
eleung32 <52718704+eleung32@users.noreply.github.com>
2021-10-07 00:04:10 -0400

    contact page setup contact1.js

---
+++ js/contact1.js
@@ -0,0 +1,20 @@
+//hide phone number
+const phoneNumber1 = "+1 416 587 0971";
+document.getElementById("phone1").innerHTML=`${phoneNumber1}`;
+// document.getElementById("callme").innerHTML=`<a href="tel:${phoneNumber}">${phoneNumber}</a>`;
+const phoneNumber2 = "+1 416 587 0971";
+document.getElementById("phone2").innerHTML=`${phoneNumber2}`;
+
+const phoneNumber3 = "+1 416.587.0971";
+document.getElementById("phone3").innerHTML=`${phoneNumber3}`;
+
+
+//hide email address
+const emailAddress1 = "rochelle@mgbmc.com";
+document.getElementById("sendmail1").innerHTML=`${emailAddress1}`;
+// document.getElementById("sendmail").innerHTML=`<a href="mailto:${emailAddress}">${emailAddress}</a>`;
+const emailAddress2 = "rochelle@mgbmc.com";
+document.getElementById("sendmail2").innerHTML=`${emailAddress2}`
+
+const emailAddress3 = "rochelle@mgbmc.com";
+document.getElementById("sendmail3").innerHTML=`${emailAddress3}`;
